1fc4b1f961b298f3c90d19107ddbbba5491a28ad
Hentaitang <mz374427166@gmail.com>
2018-11-12 17:41:20 +0800

    项目首次上传，标题栏

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <img src="..\assets\cnodejs_light.svg" alt="">
+    <ul>
+      <li>首页</li>
+      <li>新手入门</li>
+      <li>API</li>
+      <li>关于</li>
+      <li>注册</li>
+      <li>登录</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+
+<style scoped>
+.header{
+  background-color: #444;
+  height: 50px;
+}
+img{
+  max-width: 120px;
+  margin-left: 63px;
+  margin-top: 8px;
+}
+ul{
+  list-style: none;
+  color: #ccc;
+  font-size: 13px;
+  float: right;
+  margin-top: 6px;
+  margin-right: 50px;
+}
+li{
+  display: inline-block;
+  padding: 10px 15px;
+}
+</style>
